371bf87ec4a023e7a706919ab024f5b5a53dbef4
Novytska12321 <novytska@seznam.cz>
2026-08-12 21:38:53 +0200

    add empty game component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { GameComponent } from './views/game/ui/game.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'game', component: GameComponent },
 ];
